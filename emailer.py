aea2c3f154693cdef838dbfdcde685915dad37e9
pushkarbalwadkar-dev <pushkar.balwadkar@gmail.com>
2026-03-12 12:18:02 +0100

    Create emailer.py

---
+++ emailer.py
@@ -0,0 +1,32 @@
+import smtplib
+import pandas as pd
+import os
+from email.mime.text import MIMEText
+
+EMAIL = os.getenv("EMAIL_USER")
+PASSWORD = os.getenv("EMAIL_PASS")
+TO = os.getenv("EMAIL_TO")
+
+df = pd.read_csv("jobs.csv")
+
+html = "<h2>Daily SFMC Jobs</h2><ul>"
+
+for _,job in df.iterrows():
+
+    html += f"<li><b>{job['title']}</b> - {job['company']} \
+    (<a href='{job['link']}'>Apply</a>)</li>"
+
+html += "</ul>"
+
+msg = MIMEText(html,"html")
+
+msg["Subject"] = "Daily SFMC Job Report"
+msg["From"] = EMAIL
+msg["To"] = TO
+
+with smtplib.SMTP_SSL("smtp.gmail.com",465) as server:
+
+    server.login(EMAIL,PASSWORD)
+    server.sendmail(EMAIL,TO,msg.as_string())
+
+print("Email sent")
