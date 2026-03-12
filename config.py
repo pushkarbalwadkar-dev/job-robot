c146ef68e03e9640f6f53d222c1fccc6061c5f8d
pushkarbalwadkar-dev <pushkar.balwadkar@gmail.com>
2026-03-12 11:42:46 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,26 @@
+KEYWORDS = [
+    "Salesforce Marketing Cloud",
+    "SFMC Developer",
+    "Salesforce Marketing Cloud Consultant",
+    "SFMC Specialist",
+    "Salesforce Architect"
+]
+
+PREFERRED_LOCATIONS = [
+    "remote",
+    "Paris",
+    "France",
+    "europe",
+    "Netherlands",
+    "Luxembourg",
+    "Poland",
+    "usa",
+    "india",
+    "Australia",
+    "Hong Kong",
+    "Canada",
+    "Dubai",
+    "UAE"
+]
+
+MAX_JOBS_PER_SITE = 50
